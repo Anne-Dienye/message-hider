d8b0a376d75a9396344adcbd6de22b30987f693e
Anne-Dienye <dienyeucheawaji93@gmail.com>
2025-04-09 13:49:16 +0100

    Add files via upload

---
+++ Untitled-3.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env pythons 3
+
+
+def random_letters(number_of_letters=2)
+    from string import ascii_letters as letters
+    ftom the random import choice
+
+    random_string=
+    for _ in range(mumber_of_)letters:
+        random_string += choice(letters)
+
+    return_stirin
+
+    if --name__ == '---':
+
+        paddineg-leve; = randint(2,200)
+        
+        encrypted_message =
+    
+        for letter in massage:
+            encryted message += letter
+
+       encryted_message += random_letters(padling_level)                                                                                                                                                                                                                                                                                                     
